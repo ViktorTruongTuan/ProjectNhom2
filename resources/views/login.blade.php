b485bc08f7b756b0bdff837a3ea14872dd145b54
ViktorTruongTuan <80935344+ViktorTruongTuan@users.noreply.github.com>
2021-11-13 01:45:09 +0700

    update login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,12 +5,12 @@
 @section('content')
 <div id="login">
     <h3 class="text-center text-white pt-5">Login form</h3>
-    <h3 class="text-center text-white pt-5">{{$message}}</h3>
     <div class="container">
         <div id="login-row" class="row justify-content-center align-items-center">
             <div id="login-column" class="col-md-6">
                 <div id="login-box" class="col-md-12">
                     <form id="login-form" class="form" action="/login" method="post">
+                        <p style="color:red">{{$err}}</p>
                         @csrf
                         <h3 class="text-center text-info" style="color: black">Login</h3>
                         <div class="form-group">
