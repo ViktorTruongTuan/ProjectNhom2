1e37c3d843cea41c014cd0f7a58c0cb7c86725a6
Nguyen Truong Tuan <tuan0967151537@gmail.com>
2021-11-18 00:25:47 +0700

    Show ProductList

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\ProductList;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -9,6 +10,7 @@ class HomeController extends Controller
     public function GetHomePage(Request $request){
         $role = $request->session()->get('role');
         $user = $request->session()->get('user');
-        return view('HOME',['role'=>$role,'name'=>$user]);
+        $productlist = ProductList::all();
+        return view('HOME',['role'=>$role,'name'=>$user,'productlist'=>$productlist]);
     }
 }
--- resources/views/HOME.blade.php
+++ resources/views/HOME.blade.php
@@ -1,4 +1,13 @@
 @extends('master')
-
+@section('productlist')
+  <div style="padding:10px;" class="row">
+    @foreach ($productlist as $product )
+    <div class="col-3 productbox">
+        <img src="{{asset($product->PicturePath)}}" class="img-responsive">
+        <div class="producttitle">{{$product->ProductName}}</div>
+        <div class="productprice"><div class="pull-right"><a href={{'/cart/'.$product->id}} class="btn btn-danger btn-sm" role="button">BUY</a></div><div class="pricetext">{{$product->Price}}</div></div>
+    </div>
+    @endforeach</div>
+@endsection
 
 
